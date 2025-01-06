e7e9421c9d205b9bb144557b10c1fd9c0dcd4df2
ffreemt <yucongo+fmt@gmail.com>
2025-01-06 13:17:20 +0800

    Update bot part

--- marimo_tr.py
+++ marimo_tr.py
@@ -19,13 +19,15 @@ def _():
     from time import sleep
     from types import SimpleNamespace
 
+    from loguru import logger
     import pandas as pd
     import numpy as np
     from unsync import unsync
 
     import marimo as mo
 
-    # from ycecream import y
+    from ycecream import y
+    y.configure(e=0)
 
     from deeplx_tr.batch_deeplx_tr import batch_deeplx_tr
     from deeplx_tr.batch_newapi_tr import batch_newapi_tr
@@ -37,12 +39,14 @@ def _():
         batch_deeplx_tr,
         batch_newapi_tr,
         io,
+        logger,
         mo,
         np,
         pd,
         sleep,
         trtext2docx,
         unsync,
+        y,
         zip_longest,
     )
 
@@ -130,29 +134,69 @@ def _(
         ns.docx_file = _.getvalue()
         ns.df = pd.DataFrame([*zip_longest(ns.text, ns.dxtext, ns.lmtext, fillvalue='')], columns=columns)
 
+    def reset_pbar():
+        for _ in mo.status.progress_bar(
+            range(1),
+            title="remove pbar ...",
+            # completion_title="done lmtr",
+            remove_on_exit=True,
+        ):
+            ...
+        ...
 
     ns.text = ['']
     ns.dxtext = ['']
     ns.lmtext = ['']
-    ns.docx_file = Path(r'C:\Users\User\Documents\111.docx').read_bytes()
+    ns.docx_file = Path('empty.docx').read_bytes()
     ns.filename = 'temp-marimo.txt'
     ns.df = pd.DataFrame([*zip_longest(ns.text, ns.dxtext, ns.lmtext, fillvalue='')], columns=columns)
-    return columns, fileobj, gen_dxtext, gen_lmtext, ns
+
+    ns.row_tot = 1
+    ns.row_numb = 0
+    return columns, fileobj, gen_dxtext, gen_lmtext, ns, reset_pbar
 
 
 @app.cell
-def _(
-    Path,
-    button_dx,
-    button_lm,
-    columns,
-    fileobj,
-    mo,
-    ns,
-    pd,
-    y,
-    zip_longest,
-):
+def _(mo):
+    get_state, set_state = mo.state(0)  # row_numb
+
+    get_tot, set_tot = mo.state(1)  # total # of rows
+    return get_state, get_tot, set_state, set_tot
+
+
+@app.cell
+def _(get_state, mo, ns):
+    # for lmtool_tab
+    row1 = mo.ui.text_area(value=ns.text[get_state()], rows=2, full_width=True)
+    row2 = mo.ui.text_area(value=ns.dxtext[get_state()], rows=2,  full_width=True)
+    row3 = mo.ui.text_area(value=ns.lmtext[get_state()], rows=2,  full_width=True)
+    return row1, row2, row3
+
+
+@app.cell
+def _(logger, ns, row1, set_state):
+    def set_row_numb(n):
+        logger.info(f"{n=}")
+        # logger.info(ns)
+        ns.row_numb = n
+        set_state(ns.row_numb)
+
+        # row1 = ns.text[n]
+        # row2 = ns.dxtext[n]
+        # row3 = ns.lmtext[n]
+        # logger.info(row1)
+
+        # not allowed, have to use mo.state
+        # row1.value = ns.text[n]
+        # row2.value = ns.dxtext[n]
+        # row3.value = ns.lmtext[n]
+
+        logger.info(f"{row1.value=}")
+    return (set_row_numb,)
+
+
+@app.cell
+def _(columns, fileobj, ns, pd, set_state, set_tot, y, zip_longest):
     if fileobj.contents():
         lines = []
         try:
@@ -161,26 +205,70 @@ def _(
             y(e)
             raise
         ns.filename = fileobj.name()
-        ns.text = [elm.strip() for elm in lines]
+        ns.text = [elm.strip() for elm in lines if elm.strip()]
         ns.df = pd.DataFrame([*zip_longest(ns.text, ns.dxtext, ns.lmtext, fillvalue='')], columns=columns)
+
+        # update dxtext, lmtext
+        _, ns.dxtext, ns.lmtext = ns.df.values.T.tolist() 
+        ns.row_tot = len(ns.text)
+        ns.row_numb = 0
+        set_state(ns.row_numb)
+        set_tot(ns.row_tot)
+    return (lines,)
+
+
+@app.cell
+def _(
+    Path,
+    button_dx,
+    button_lm,
+    button_reset_pbar,
+    fileobj,
+    get_state,
+    get_tot,
+    logger,
+    mo,
+    ns,
+    row1,
+    row2,
+    row3,
+    set_row_numb,
+):
+    # _ = slider_row_numb.value
+    slider_row_numb = mo.ui.slider(
+        start=0,
+        stop=ns.row_tot - 1,
+        value=get_state(),
+        step=1,
+        label=f"row-{get_state()}/{get_tot()}",
+        show_value=True,
+        on_change=set_row_numb,
+        # on_change=set_state,
+        full_width=True,
+    )
 
     # ns.docx_file = Path(r'C:\Users\User\Documents\111.docx').read_bytes()
     # _ = f'{Path(ns.filename).stem}-tr.docx'
+    try:
+        filename_ = f'{Path(ns.filename).stem}-tr.docx'
+        ns.filename = filename_
+    except Exception as e:
+        logger.error(e)
+        filename_ = ns.filename
+        logger.info(f'{ns.filename=}, {filename_=}')
 
     button_dl = mo.download(
         data=ns.docx_file,
-        filename=f'{Path(ns.filename).stem}-tr.docx',
-        # mimetype="text/plain",
-        # mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        filename=filename_,
         label="dl docx",
     )
 
     file_tab = mo.vstack([
         # mo.hstack([fileobj]),
         mo.hstack(
-            [fileobj, button_dx, button_lm, button_dl,],
+            [fileobj, button_dx, button_lm, button_reset_pbar, button_dl,],
             align='start',
-            widths=[4, 1, 1, 1,]
+            widths=[4, 1, 1, 1, 1]
         ),
         mo.accordion({
             "...": mo.ui.table(
@@ -195,9 +283,18 @@ def _(
             )
         }),
     ])
-    # file_tab
 
-    return button_dl, file_tab, lines
+    # _ = ns  # react to ns? to update lmtool_tab
+
+    lmtool_tab = mo.vstack(
+        [
+            slider_row_numb,
+            row1,
+            row2,
+            row3,
+        ]
+    )
+    return button_dl, file_tab, filename_, lmtool_tab, slider_row_numb
 
 
 @app.cell
@@ -210,15 +307,6 @@ def _(mo):
         ]
     )
 
-    organization = mo.vstack(
-        [
-            mo.md("Edit Organization"),
-            org := mo.ui.text(label="Organization Name", value="..."),
-            employees := mo.ui.number(
-                label="Number of Employees", start=0, stop=1000
-            ),
-        ]
-    )
 
     info = mo.vstack(
         [
@@ -235,11 +323,11 @@ def _(mo):
 
     mo.sidebar(
         [
-            # mo.md("## llmtool"),
+            # mo.md("## lmtool"),
             mo.nav_menu(
                 {
                     "#/": f"{mo.icon('lucide:file')} File",
-                    "#/llmtool": f"{mo.icon('lucide:bot')} Llmtool",
+                    "#/lmtool": f"{mo.icon('lucide:bot')} Lmtool",
                     "#/info": f"{mo.icon('lucide:info')} Info",
                     # "Links": {
                     #     "https://twitter.com/marimo_io": "Twitter",
@@ -250,16 +338,16 @@ def _(mo):
             ),
         ]
     )
-    return employees, first, info, last, org, organization, settings
+    return first, info, last, settings
 
 
 @app.cell
-def _(file_tab, info, mo, organization):
+def _(file_tab, info, lmtool_tab, mo):
     mo.routes({
         # "#/": mo.md("# Home"),
         # "#/": settings,
         "#/": file_tab,
-        "#/llmtool": organization,
+        "#/lmtool": lmtool_tab,
         "#/info": info,
         mo.routes.CATCH_ALL: file_tab,
     })
@@ -267,8 +355,7 @@ def _(file_tab, info, mo, organization):
 
 
 @app.cell
-def _(gen_dxtext, gen_lmtext, mo):
-
+def _(gen_dxtext, gen_lmtext, mo, reset_pbar):
     button_dx = mo.ui.run_button(
         label='dxtr',
         kind='success',
@@ -283,7 +370,103 @@ def _(gen_dxtext, gen_lmtext, mo):
         on_change=lambda _: gen_lmtext()
     )
 
-    return button_dx, button_lm
+    button_reset_pbar = mo.ui.run_button(
+        label='🚮',
+        kind='warn',
+        tooltip='click to remove progressbar',
+        on_change=lambda _: reset_pbar()
+    )
+    return button_dx, button_lm, button_reset_pbar
+
+
+@app.cell
+def _(mo):
+    import os
+
+    os.environ.update(
+        OPENAI_BASE_URL='https://litellm.dattw.eu.org/v1',
+    )
+
+    get_model, set_model = mo.state('gpt-4o-mini')
+    return get_model, os, set_model
+
+
+@app.cell
+def _(get_model, mo, set_model):
+    model_list = [
+        "gpt-4o-mini", 
+        "gemini-1.5-flash", "gemini-1.5-pro", 
+        "gemini-2.0-flash-exp", 
+        "deepseek-chat", "deepseek-v3",
+    ]
+
+    dropdown_model_sel = mo.ui.dropdown(
+        options=model_list, 
+        value="gpt-4o-mini", 
+        label="model: ",
+        on_change=set_model,
+    )
+
+    oai = mo.ai.llm.openai(
+        get_model(),
+        # 'gpt-4o-mini',
+        system_message='You are a helpful assistant',
+    )
+    return dropdown_model_sel, model_list, oai
+
+
+@app.cell
+def _(dropdown_model_sel, mo, ns, oai, y):
+
+    def my_model(messages, config):
+        question = messages[-1].content
+        y(question)
+        
+        # Search for relevant docs in a vector database, blog storage, etc.
+        # docs = find_relevant_docs(question)
+        # context = "\n".join(docs)
+        # context = '''我叫老王。我来自北京'''
+        context = f'''
+            <原文:>{ns.text[ns.row_numb]}</原文:> 
+
+            <dx译文:>{ns.dxtext[ns.row_numb]}</dx译文:>
+
+            <lm译文:>{ns.lmtext[ns.row_numb]}</lm译文:>
+        '''
+        y(context)
+        prompt = f"{context}\n\nQuestion: {question}\n\nAnswer:"
+        # Query your own model or third-party models
+        # response = query_llm(prompt, config)
+        response = oai([mo.ai.llm.ChatMessage(role='user', content=prompt)], config)
+
+        return response
+
+    bot = mo.vstack(
+        [
+            dropdown_model_sel,
+            mo.ui.chat(
+              my_model,
+              prompts=[
+                "{{phrase}} 什么意思，怎么翻译",
+                "列出 {{phrase}} 的 5 个中文同义词", 
+                  "根据原文和dx译文给出修改后的翻译，用黑体标出修改过的部分",
+                  "根据原文和lm译文给出修改后的翻译",
+                  "根据原文和dx译文及lm译文给出修改后的翻译",
+                  "根据原文和dx译文提出修改dx译文的建议",
+                  "根据原文和dx译文提出修改dx译文的建议并给出修改后的翻译，用黑体标出修改过的部分",
+                  "根据原文和lm译文提出修改lm译文的建议",
+                  "根据原文和lm译文提出修改lm译文的建议并给出修改后的翻译，用黑体标出修改过的部分",
+                  "根据原文和dx译文及lm译文提出修改建议并给出修改后的翻译，用黑体标出修改过的部分",
+                "Say this is a test.",  
+              ],
+              show_configuration_controls=True,
+              max_height=400,
+            )
+        ]
+    )
+
+    bot
+    return bot, my_model
 
 
 if __name__ == "__main__":
